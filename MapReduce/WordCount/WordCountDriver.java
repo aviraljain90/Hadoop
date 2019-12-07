e84d72a23b064530cdd1e00d163282e77f0012e1
aviraljain90 <jain.aviral90@gmail.com>
2019-12-07 10:50:51 +0530

    Create WordCountDriver.java

---
+++ MapReduce/WordCount/WordCountDriver.java
@@ -0,0 +1,32 @@
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.fs.*;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+
+    public static void main(String[] args)
+    throws IOException,InterruptedException,ClassNotFoundException
+    {
+        JobConf conf = new JobConf();
+        Job job = new Job(conf,"WordCount");
+
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.addInputPath(conf, new Path("/user/abc.txt"));
+        FileOutputFormat.setOutputPath(conf,new Path("/user"));
+
+        Boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+    }
+}
